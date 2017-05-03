93476b3bc19c6740907c144ab0bcbfd53115d1d7
Kevin Berthier <kevinberthier@orange.fr>
2017-05-03 12:38:47 +0200

    back fonctionnel

--- app/controllers/coachings_controller.rb
+++ app/controllers/coachings_controller.rb
@@ -3,5 +3,14 @@ class CoachingsController < ApplicationController
   end
 
   def answer
+    @your_message = params[:query]
+    @coach_answer =
+    if @your_message.downcase == "i am going to work right now!"
+      ''
+    elsif @your_message.end_with?("?")
+      "Silly question, get dressed and go to work!"
+    else
+      "I don't care, get dressed and go to work!"
+    end
   end
 end
